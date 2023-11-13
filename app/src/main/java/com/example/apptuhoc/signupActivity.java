d4c64ce4676a5570452196ae04cb36aef18f09ac
TruongBino <110027416+TruongBino@users.noreply.github.com>
2023-11-13 15:13:26 +0700

    thêm layout sign up , splash , main

---
+++ app/src/main/java/com/example/apptuhoc/signupActivity.java
@@ -0,0 +1,26 @@
+package com.example.apptuhoc;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.apptuhoc.databinding.ActivitySignupBinding;
+
+public class signupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
